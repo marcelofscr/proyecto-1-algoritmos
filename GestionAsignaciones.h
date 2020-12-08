e579c779441f440be55fa0336012be6bdf0419fe
94savz <75226934+94savz@users.noreply.github.com>
2020-12-07 19:40:27 -0600

    Update GestionAsignaciones.h

--- GestionAsignaciones.h
+++ GestionAsignaciones.h
@@ -1,8 +1,8 @@
-//Marcelo Fern·ndez Solano
-//Steven Vega ZuÒiga
+//Marcelo Fern√°ndez Solano
+//Steven Vega Zu√±iga
 //TEC-Requerimientos
-/* DescripciÛn: TEC-Requerimientos es una solucion computacional creada con el fin de gestionar los miembros y 
-				requerimientos de un proyecto, asi mismo permite la creacion de asignaciones asiciadas con un 
+/* Descripci√≥n: TEC-Requerimientos es una solucion computacional creada con el fin de gestionar los miembros y 
+				requerimientos de un proyecto, asi mismo permite la creacion de asignaciones asociadas con un 
 				miembro y un recurso, y el registro de incidentes */
 				
 #include <stdio.h>
